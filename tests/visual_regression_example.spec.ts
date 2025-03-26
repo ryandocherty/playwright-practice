eff4eb85439b0861e131c6c79a4d9f17426eaf00
Ryan Docherty <ryandocherty95@gmail.com>
2025-03-26 14:16:42 +0000

    Changes to be committed: 	modified:   playwright.config.ts     >trace: "on", just so I can get used to using the trace viewer 	new file:   tests/trace_viewer_example_001.spec.ts     >This test fails on purpose, so demo the trace viewer 	new file:   tests/visual_regression_example.spec.ts     >Simple demo of a visual regression test 	new file:   tests/visual_regression_example.spec.ts-snapshots/saucedemo-landing-page-chromium-win32.png 	new file:   tests/visual_regression_example.spec.ts-snapshots/saucedemo-logged-in-chromium-win32.png

---
+++ tests/visual_regression_example.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test("Visual regression example", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/v1/");
+
+  /*
+Using expect(page).toHaveScreenshot() helps maintain consistent UI
+by checking for unintended visual changes across builds.
+This is particularly useful when changed are made to the frontend code,
+ensuring that the application's appearence remains as intended.
+*/
+
+  //Take a screenshot of the initial state of the landing page:
+  await expect(page).toHaveScreenshot("saucedemo-landing-page.png");
+  //This line checks the current page's screenshot matches a baseline screenshot named 'saucedemo-landing-page.png'
+  //If a baseline image does not exist, it will create one
+
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+
+  //Take another screenshot of the initial state after logging in:
+  await expect(page).toHaveScreenshot("saucedemo-logged-in.png");
+});
+
+/*
+-----Managing Baseline Screenshots-----
+
+Initial Run:
+The first time yoy run the test, it will save the screenshots as baseline images.
+
+Re-running the Test:
+If the layout of the application changes and you want to update the baseline,
+you can do so by deleting the existing snapshots. This way, Playwright will
+create new baseline screenshots based on the latest UI state.
+*/
