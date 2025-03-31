3d98b3b469320d4e4705fd549eaa923fae31d163
Ryan Docherty <ryandocherty95@gmail.com>
2025-03-31 15:24:40 +0100

    Changes to be committed: 	new file:   tests/helpers_saucedemo.ts     >Helper file for login_test_saucedemo.spec.ts, to help with code duplication     >Contains some defined selectors, and a function to get text content from any given webpage element 	new file:   tests/login_test_saucedemo.spec.ts

---
+++ tests/helpers_saucedemo.ts
@@ -0,0 +1,21 @@
+//----------This is a helper file for login_test_saucedemo.spec.ts----------
+
+import { Page } from "playwright/test";
+
+//declare constants for various selectors on saucedemo.com:
+export const SELECTORS = {
+  loginButton_Saucedemo: '[data-test="login-button"]',
+  errorMessage_Saucedemo: '[data-test="error"]',
+  usernameInputBox_Saucedemo: '[data-test="username"]',
+  passwordInputBox_Saucedemo: '[data-test="password"]',
+};
+
+//Function to extract text from any of the elements defined above:
+export async function getElementText(page: Page, selector: string) {
+  return await page.evaluate((selec) => {
+    //Query the given element:
+    const element = document.querySelector(selec);
+    //If the element is found, return the text content. If not found, return an error
+    return element ? element?.textContent : "Element not found";
+  }, selector);
+}
---
+++ tests/login_test_saucedemo.spec.ts
@@ -0,0 +1,77 @@
+"use strict";
+import { test, expect } from "@playwright/test";
+import { getElementText, SELECTORS } from "./helpers_saucedemo";
+
+/*
+These tests are to check that the error messages displayed
+on saucedemo.com are correct, depending on which combination
+of login credentials are given.
+*/
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.waitForLoadState("networkidle");
+});
+
+test("Saucedemo invalid login: no credentials", async ({ page }) => {
+  //attempt to login without entering any credentials:
+  await page.locator(SELECTORS.loginButton_Saucedemo).click();
+  await expect(page.locator(SELECTORS.errorMessage_Saucedemo)).toBeVisible();
+
+  //1. Call the getElementText() function (defined in helpers.ts)
+  //2. Pass 1st arg: the 'page' fixture
+  //3. Pass 2nd arg: the specified selector (defined in helpers.ts)
+  expect(await getElementText(page, SELECTORS.errorMessage_Saucedemo)).toBe("Epic sadface: Username is required");
+});
+
+test("Saucedemo invalid login: no password", async ({ page }) => {
+  //attempt to login with correct username + no password:
+  await page.locator(SELECTORS.usernameInputBox_Saucedemo).fill("standard_user");
+  await page.locator(SELECTORS.loginButton_Saucedemo).click();
+  await expect(page.locator(SELECTORS.errorMessage_Saucedemo)).toBeVisible();
+  expect(await getElementText(page, SELECTORS.errorMessage_Saucedemo)).toBe("Epic sadface: Password is required");
+});
+
+test("Saucedemo invalid login: no username", async ({ page }) => {
+  //attempt to login with no username + correct password:
+  await page.locator(SELECTORS.passwordInputBox_Saucedemo).fill("secret_sauce");
+  await page.locator(SELECTORS.loginButton_Saucedemo).click();
+  await expect(page.locator(SELECTORS.errorMessage_Saucedemo)).toBeVisible();
+  expect(await getElementText(page, SELECTORS.errorMessage_Saucedemo)).toBe("Epic sadface: Username is required");
+});
+
+test("Saucedemo invalid login: invalid username", async ({ page }) => {
+  //attempt to login with incorrect username:
+  await page.locator(SELECTORS.usernameInputBox_Saucedemo).fill("not_a_valid_username");
+  //this is the valid password:
+  await page.locator(SELECTORS.passwordInputBox_Saucedemo).fill("secret_sauce");
+  await page.locator(SELECTORS.loginButton_Saucedemo).click();
+  await expect(page.locator(SELECTORS.errorMessage_Saucedemo)).toBeVisible();
+  expect(await getElementText(page, SELECTORS.errorMessage_Saucedemo)).toBe(
+    "Epic sadface: Username and password do not match any user in this service"
+  );
+});
+
+test("Saucedemo invalid login: invalid password", async ({ page }) => {
+  //attempt to login with correct username:
+  await page.locator(SELECTORS.usernameInputBox_Saucedemo).fill("standard_user");
+  //this is an invalid password:
+  await page.locator(SELECTORS.passwordInputBox_Saucedemo).fill("no_a_password");
+  await page.locator(SELECTORS.loginButton_Saucedemo).click();
+  await expect(page.locator(SELECTORS.errorMessage_Saucedemo)).toBeVisible();
+  expect(await getElementText(page, SELECTORS.errorMessage_Saucedemo)).toBe(
+    "Epic sadface: Username and password do not match any user in this service"
+  );
+});
+
+test("Saucedemo invalid login: locked_out_user", async ({ page }) => {
+  //attempt to login with username for someone who is locked out:
+  await page.locator(SELECTORS.usernameInputBox_Saucedemo).fill("locked_out_user");
+  //this is a valid password:
+  await page.locator(SELECTORS.passwordInputBox_Saucedemo).fill("secret_sauce");
+  await page.locator(SELECTORS.loginButton_Saucedemo).click();
+  await expect(page.locator(SELECTORS.errorMessage_Saucedemo)).toBeVisible();
+  expect(await getElementText(page, SELECTORS.errorMessage_Saucedemo)).toBe(
+    "Epic sadface: Sorry, this user has been locked out."
+  );
+});
