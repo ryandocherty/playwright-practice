cf1f3098e6bcdf2c6be57ff2ffa61940aceae6ae
Ryan Docherty <ryandocherty95@gmail.com>
2025-04-15 13:48:34 +0100

    Changes to be committed: 	modified:   tests/udemy-tutorials/Section_4_13_Extract_MultipleWebElements.spec.ts     >Udemy tutorial: extracting multiple web elements using Parent & Child classes to traverse the webpage.

--- tests/udemy-tutorials/Section_4_13_Extract_MultipleWebElements.spec.ts
+++ tests/udemy-tutorials/Section_4_13_Extract_MultipleWebElements.spec.ts
@@ -2,7 +2,7 @@
 
 import { test, expect } from "@playwright/test";
 
-test("Udemy: Extracting Text Demo", async ({ browser }) => {
+test("Udemy: Extracting Multiple Elements", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
@@ -11,17 +11,55 @@ test("Udemy: Extracting Text Demo", async ({ browser }) => {
   const passwordInputBox = page.locator(`[type="password"]`);
   const signInButton = page.locator(`#signInBtn`);
 
-  //--------------------Error message demo--------------------
-  await userNameInputBox.fill(`Wrong_Username`);
-  await passwordInputBox.fill(`Wrong_Password`);
-  await signInButton.click();
-  const errorMessageText = await page.locator(`[style*="block"]`).textContent();
-  console.log(`Error message: "${errorMessageText}"`);
-  expect(errorMessageText).toContain(`Incorrect`);
-  //----------------------------------------------------------
-
   await userNameInputBox.clear();
   await passwordInputBox.clear();
   await userNameInputBox.fill(`rahulshettyacademy`);
   await passwordInputBox.fill(`learning`);
+  await signInButton.click();
+
+  /*
+  For this test I want to grab the title of the first product (iPhone X) on the page.
+
+  The way the HTML is set up is as follows:
+
+  <div class="card-body">
+    <h4 class="card-title">
+        <a href="#" xpath="1">iphone X</a>
+    </h4>
+  </div>
+
+  I'll need to use .className for the selector, 
+  but then traverse from Parent to Child to grab the actual title text.
+
+  The Parent here is ".card-title" (can also use "card-body" though).
+  There aren't any specific attributes to use on the line containing "iphone X",
+  so instead I can give the HTML tag name "a" (meaning "anchor").
+
+  Syntax = .className <tag>
+
+  So this would read as ".card-title a"
+
+  However, on this specific website there are 4 products that share these
+  same class names and tags. So this would fail, as Playwright wouldn't know
+  which element to return.
+
+  One way to specify the first product would be to use "nth()":
+  await page.locator(".card-title a").nth(0).textContent();
+
+  Using this method, all 4 of the elements found will be taken into an array.
+
+  Another way to specify the first product would be to use "first()":
+  await page.locator(".card-title a").first().textContent();
+
+  Note - there isn't a second() or third() etc., but there is a last().
+  To get the in-between elements, use nth(1), nth(2) etc.
+  */
+
+  const firstProductTitle = await page.locator(`.card-title a`).first().textContent();
+  console.log(`1st Product: "${firstProductTitle}"`);
+  expect(firstProductTitle).toBe(`iphone X`);
+
+  const secondProductTitle = await page.locator(`.card-title a`).nth(1).textContent();
+  console.log(`2nd Product: "${secondProductTitle}"`);
+  expect(secondProductTitle).toBe(`Samsung Note 8`);
 });
