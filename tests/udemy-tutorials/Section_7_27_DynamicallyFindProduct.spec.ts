d79b3ae8df52d3a3c14191ad1ad8e5a80f97c7e4
Ryan Docherty <ryandocherty95@gmail.com>
2025-04-24 13:17:55 +0100

    Changes to be committed: 	modified:   tests/udemy-tutorials/Section_7_27_DynamicallyFindProduct.spec.ts     >Added functionality to grab the item's price before and after adding it to the cart.     >Added an expect() to assert that these two values match.

--- tests/udemy-tutorials/Section_7_27_DynamicallyFindProduct.spec.ts
+++ tests/udemy-tutorials/Section_7_27_DynamicallyFindProduct.spec.ts
@@ -20,6 +20,7 @@ test("Udemy: Client Item Purchase Test", async ({ browser }) => {
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/client");
 
+  /*-------------------------Import email address and password-------------------------*/
   //Assign the email and password to a variable.
   //Using the nullish coalescing operator "??" to provide a fallback/default value,
   //in case there is an issue importing them:
@@ -31,12 +32,14 @@ test("Udemy: Client Item Purchase Test", async ({ browser }) => {
     console.log(`LOGIN_EMAIL: ${loginEmail}`);
     console.log(`LOGIN_PASSWORD: ${loginPassword}`);
   }
+  /*-----------------------------------------------------------------------------------*/
 
-  //Login to the shop:
+  /*--------------------------------Log in to the shop---------------------------------*/
   await page.fill(SELECTORS_CLIENT.userEmailInput, loginEmail);
   await page.fill(SELECTORS_CLIENT.userPasswordInput, loginPassword);
   await page.locator(SELECTORS_CLIENT.loginButton).click();
   await expect(page).toHaveURL(`https://rahulshettyacademy.com/client/dashboard/dash`);
+  /*-----------------------------------------------------------------------------------*/
 
   const products = page.locator(`.card-body`); //returns an array.
   await page.waitForLoadState(`networkidle`); //without this, productTitles below weren't being grabbed.
@@ -46,6 +49,11 @@ test("Udemy: Client Item Purchase Test", async ({ browser }) => {
   console.log(`Number of products found: ${productsAmount}`);
   console.log(productTitles);
 
+  //Initialise a variable to hold the item's displayed price before adding to cart:
+  //Also initialising with a default value, otherwise I get an error
+  //later on when comparing priceBeforeCart to priceInCart.
+  let priceBeforeCart_Numeric: number | null = null;
+
   //Chained locator using the "products" array:
   //"products" uses the ".card-body" class to loop through the products at position "i",
   //then locates the corresponding "b" attribute (product names/text).
@@ -54,8 +62,14 @@ test("Udemy: Client Item Purchase Test", async ({ browser }) => {
   for (let i = 0; i < productsAmount; ++i) {
     if ((await products.nth(i).locator(`b`).textContent()) === `ZARA COAT 3`) {
       await expect(products.nth(i).getByRole(`button`, { name: ` Add To Cart` })).toBeVisible();
+
+      const priceBeforeCart: any = await products.nth(i).locator(`.text-muted`).textContent();
+      priceBeforeCart_Numeric = parseFloat(priceBeforeCart?.replace(/[^0-9.-]+/g, ""));
+
+      console.log(`Item Price (before cart): $${priceBeforeCart_Numeric}`);
+
       await products.nth(i).getByRole(`button`, { name: ` Add To Cart` }).click();
-      console.log(`Clicked 'Add To Cart'.`);
+      console.log(`Clicked 'Add To Cart'`);
       break;
     }
   }
@@ -63,10 +77,19 @@ test("Udemy: Client Item Purchase Test", async ({ browser }) => {
   //Click the 'Cart' button:
   await page.locator(`[routerlink='/dashboard/cart']`).click();
 
-  //Grab the text of the itemInCart:
+  //Grab the text of the current item in cart:
   const itemInCart = await page.locator(`div[class='cartSection'] h3`).textContent();
 
-  //Assert the correct item (ZARA COAT 3) is in the cart:
+  //Grab the displayed price of the current item in cart:
+  const priceInCart: any = await page.locator(`.prodTotal`).textContent();
+  const priceInCart_Numeric: number = parseFloat(priceInCart?.replace(/[^0-9]+/g, ""));
+
   console.log(`Item in cart: ${itemInCart}`);
+  console.log(`Item Price (in cart): $${priceInCart_Numeric}`);
+
+  //Assert the correct item (ZARA COAT 3) is in the cart:
   expect(itemInCart).toBe(`ZARA COAT 3`);
+
+  //Assert that the price before adding to cart matches the price displayed in the cart:
+  expect(priceBeforeCart_Numeric).toEqual(priceInCart_Numeric);
 });
