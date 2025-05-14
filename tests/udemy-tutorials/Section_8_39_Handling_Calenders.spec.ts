86f7699acfe7ca6998f0a3244d5dc8fc65cce7dd
Ryan Docherty <ryandocherty95@gmail.com>
2025-05-14 12:33:41 +0100

    Changes to be committed: 	modified:   tests/udemy-tutorials/Section_8_39_Handling_Calenders.spec.ts     >Began writing a test to learn how to handle calender automation.

--- tests/udemy-tutorials/Section_8_39_Handling_Calenders.spec.ts
+++ tests/udemy-tutorials/Section_8_39_Handling_Calenders.spec.ts
@@ -6,6 +6,16 @@ test("Udemy: Handling Calenders", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
+  await page.goto("https://rahulshettyacademy.com/seleniumPractise");
+
+  //Open the "Top Deals" page
+  const [page_TopDeals] = await Promise.all([
+    context.waitForEvent(`page`),
+    page.getByRole(`link`, { name: `Top Deals` }).click(),
+  ]);
+
+  await expect(page_TopDeals).toHaveURL(`https://rahulshettyacademy.com/seleniumPractise/#/offers`);
+
   /*------------------------------------Close Browser--------------------------------------------*/
   /*---------------------------------------------------------------------------------------------*/
 
