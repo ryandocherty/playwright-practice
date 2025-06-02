ee6d915a51086393d3416dabedf3d94d3938095d
Ryan Docherty <ryandocherty95@gmail.com>
2025-06-02 13:32:42 +0100

    Changes to be committed: 	modified:   tests/udemy-tutorials/Section_010_051_API_Utils_Refactor.spec.js     >This test is a refactor of "Section_010_046_API_Calls_Request_Response_Method.spec".     >I've set up a Utils file to handle the API calls and imported it to this file.     >This file only contains the actual UI test to "Verify if an order shows up in the order history page". 	modified:   tests/utils/Udemy_APIUtils.js     >This Utils file contains a Class to:     >1. Make an API login call which grabs and returns a login session token ("loginToken").     >2. Make an API call to place an order, using the login session token ("loginToken").     >3. Retrieve an order ID ("orderID") from placing an order, which will then be used to perform the test.     >4. Return an object ("prerequisiteData") which contains both "loginToken" and "orderID" properties/values.

--- tests/udemy-tutorials/Section_010_051_API_Utils_Refactor.spec.js
+++ tests/udemy-tutorials/Section_010_051_API_Utils_Refactor.spec.js
@@ -1,12 +1,17 @@
 //Section 10.51: Refactor API calls from utils folder and isolate from Web test logic
+//Section 10.52: Part 2 - Refactor API calls from utils folder and isolate from Web test logic
 
-//For this test I want to:
-//Rewrite "Section_010_046_API_Calls_Request_Response_Method.spec" to set up a Utils file "Udemy_APIUtils.js",
-//which I'll place the code for the API calls (login, get token, place order with token).
-//This will help keep the tests blocks seperate from any code that will be reused frequently (API calls).
+/*
+For this test:
+1. The ultimate goal is still to "Verify if an order shows up in the order history page".
+2. I'm rewriting "Section_010_046_API_Calls_Request_Response_Method", but import a Utilties file.
+3. In this isolated Utils file "Udemy_APIUtils" I'll set up Classes to handle the API calls.
+4. The API calls will be a "login" and "place order" call, which are prerequesites to obtain an orderID.
+5. I'll then perform normal UI automation to verify that the orderID appears on the order history page.
+*/
 
 import { test, expect, request } from "@playwright/test";
-//import { Udemy_APIUtils } from "./ut";
+import { Udemy_APIUtils } from "../utils/Udemy_APIUtils";
 import dotenv from "dotenv";
 
 //Load hidden environment variables:
@@ -17,27 +22,32 @@ const loginEmail = process.env.LOGIN_EMAIL ?? "";
 const loginPassword = process.env.LOGIN_PASSWORD ?? "";
 
 //Global variables:
-let loginToken;
-let orderID;
+let prerequisiteData;
 const loginPayload = { userEmail: loginEmail, userPassword: loginPassword };
 const placeOrderPayload = { orders: [{ country: "United Kingdom", productOrderedId: "67a8dde5c0d3e6622a297cc8" }] };
 
 test.beforeAll(async () => {
-  //It's better to create the APIContext in your actual test block(s), not in the Utils file.
-  //We'll pass the APIContext to a Constructor in our Utils file.
-
-  const APIContext = await request.newContext();
-});
-
-test("Udemy: Login using API", async ({ page }) => {
   /*---------------------------------------Invoke APIUtils Class---------------------------------*/
   /*---------------------------------------------------------------------------------------------*/
 
-  //const APIUtils = new
+  //It's better to create the APIContext in your actual test block(s), not in the Utils file.
+  //We'll pass the APIContext to the Constructor in our Utils file.
+  const APIContext = await request.newContext();
 
+  //Each instance of this whole test requires you to login first,
+  //so we create an object of "Udemy_APIUtils", and then pass "APIContext" and "loginPayload" to the Constructor:
+  const APIUtils = new Udemy_APIUtils(APIContext, loginPayload);
+
+  //We'll then use the "APIUtils" Class instance to call getOrderID() and pass it the "placeOrderPayload".
+  //getOrderID() returns an object "prerequisiteData" which contains the values of "loginToken" and "orderID":
+  prerequisiteData = await APIUtils.getOrderID(placeOrderPayload);
+});
+
+test("Udemy: Login using APIUtils", async ({ page }) => {
+  //Set the "loginToken" from our "prerequisiteData" object:
   await page.addInitScript((value) => {
     window.localStorage.setItem(`token`, value);
-  }, loginToken);
+  }, prerequisiteData.loginToken);
 
   await page.goto("https://rahulshettyacademy.com/client");
 
@@ -54,5 +64,5 @@ test("Udemy: Login using API", async ({ page }) => {
   await page.getByRole(`heading`, { name: `Your Orders` }).waitFor();
 
   //Assert that the "orderID" value is visible on the Order History page:
-  expect(page.getByText(orderID).first()).toBeVisible();
+  expect(page.getByText(prerequisiteData.orderID).first()).toBeVisible();
 });
--- tests/utils/Udemy_APIUtils.js
+++ tests/utils/Udemy_APIUtils.js
@@ -1,3 +1,14 @@
+//Section 10.51: Refactor API calls from utils folder and isolate from Web test logic
+//Section 10.52: Part 2 - Refactor API calls from utils folder and isolate from Web test logic
+
+/*
+This Utils file contains a Class to:
+1. Make an API login call which grabs and returns a login session token ("loginToken").
+2. Make an API call to place an order, using the login session token ("loginToken").
+3. Retrieve an order ID ("orderID") from placing an order, which will then be used to perform a test.
+4. Return an object ("prerequisiteData") which contains both "loginToken" and "orderID" properties/values.
+*/
+
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
@@ -5,17 +16,9 @@ dotenv.config({ path: ".env" });
 const loginEmail = process.env.LOGIN_EMAIL ?? "";
 const loginPassword = process.env.LOGIN_PASSWORD ?? "";
 
-//Global variables:
-let loginToken;
-let orderID;
-
 export class Udemy_APIUtils {
-  APIContext;
-  loginPayload;
-  placeOrderPayload;
-
   //Constructors are automatically invoked when a new instance of an object is created using a Class.
-  constructor(APIContext, loginPayload, placeOrderPayload) {
+  constructor(APIContext, loginPayload) {
     //The "this" keyword acts as a dynamic reference that changes based on the object we're currently interacting with.
     //The value of "this" is determined by how a method is called and its context at that point in time.
     //The Constructor will get passed "APIContext" from our test block,
@@ -24,32 +27,43 @@ export class Udemy_APIUtils {
     //Even though "this" is within the Constructor scope, using "this" lets the entire Class have access to it.
     //This means we can use "APIContext" anywhere within this Class as long as we use "this.APIContext".
 
-    //The same goes for "loginPayload" and "placeOrderPayload":
+    //Each instance of the test being run will require you to login
+    //So we'll also force the "loginPayload" to be passed as part of the constructor:
     this.loginPayload = loginPayload;
-    this.placeOrderPayload = placeOrderPayload;
   }
 
-  async getLoginToken(loginPayload) {
+  async getLoginToken() {
     const loginResponse = await this.APIContext.post(`https://rahulshettyacademy.com/api/ecom/auth/login`, {
       data: this.loginPayload,
     });
     const loginResponse_JSON = await loginResponse.json();
     console.log(loginResponse_JSON);
-    loginToken = loginResponse_JSON.token;
+    const loginToken = await loginResponse_JSON.token;
     return loginToken;
   }
 
   async getOrderID(placeOrderPayload) {
+    //Initialise an empty object to eventually hold the values for "loginToken" and "orderID" properties:
+    let prerequisiteData = {};
+    //Add a "loginToken" property and assign it the value received from the getLoginToken() method:
+    prerequisiteData.loginToken = await this.getLoginToken();
+
     const placeOrderResponse = await this.APIContext.post(
       `https://rahulshettyacademy.com/api/ecom/order/create-order`,
       {
-        data: this.placeOrderPayload,
-        headers: { Authorization: this.getLoginToken(this.loginPayload), "Content-Type": "application/json" },
+        data: placeOrderPayload,
+        headers: { Authorization: prerequisiteData.loginToken, "Content-Type": "application/json" },
       }
     );
     const placeOrderResponse_JSON = await placeOrderResponse.json();
     console.log(placeOrderResponse_JSON);
-    orderID = placeOrderResponse_JSON.orders[0];
-    return orderID;
+    const orderID = await placeOrderResponse_JSON.orders[0];
+    console.log(orderID);
+
+    //Add a "orderID" property and assign it the value received from the above "orderID":
+    prerequisiteData.orderID = orderID;
+
+    //Return the "prerequisiteData" object, which now contains the values of "loginToken" and "orderID":
+    return prerequisiteData;
   }
 }
