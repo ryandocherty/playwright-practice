ee2cc98d0ae2cf7d9d379316f9f2d646ce5ffa17
Ryan Docherty <ryandocherty95@gmail.com>
2025-07-25 13:16:28 +0100

    Changes to be committed: 	modified:   .gitignore 	modified:   playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,20 +13,67 @@ export default defineConfig({
     screenshot: "on",
   },
 
+  //Here you can parameterise different projects with different configuration settings:
+  //In the terminal, use "npx playwright test test.spec.js --project=[projectname]"
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: { browserName: "chromium", headless: true, screenshot: "on", trace: "retain-on-failure" },
     },
 
     {
       name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      use: { browserName: "firefox", headless: false, screenshot: "on", trace: "retain-on-failure" },
     },
 
     {
       name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      use: {
+        browserName: "webkit",
+        headless: false,
+        screenshot: "on",
+        trace: "retain-on-failure",
+
+        //You can simulate specific viewports:
+        viewport: { width: 720, height: 720 },
+      },
+    },
+
+    {
+      name: "Pixel7",
+      use: {
+        browserName: "chromium",
+        headless: true,
+        screenshot: "on",
+        trace: "off",
+
+        //You can simulate a device to run a test with:
+        ...devices["Pixel 7"],
+
+        //As well as screenshots, you can capture videos of test execution:
+        //It's not recommended to turn this option on for everything, as it uses lots of resources.
+        video: "on-first-retry",
+      },
+    },
+
+    {
+      name: "iPhone11",
+      use: {
+        browserName: "webkit",
+        headless: false,
+        screenshot: "on",
+        trace: "on",
+        ...devices["iPhone 11"],
+
+        //This option can be used to ignore SSL (Secure Sockets Layer) certification.
+        //Without an SSL certificate, you'll get a "This website is not secure" error.
+        //This option will help to automatically ignore the error and proceed to the website:
+        ignoreHTTPSErrors: true,
+
+        //This option can be used to automatically grant permissions to a browser.
+        //Permissions include geolocation, camera, microphone, notifications.
+        permissions: ["notifications"],
+      },
     },
   ],
 });
