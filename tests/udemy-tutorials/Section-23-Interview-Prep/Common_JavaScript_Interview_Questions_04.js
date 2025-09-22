2017ce17a4060aa1d6f0e0e25acf0ee47bf6c65f
Ryan Docherty <ryandocherty95@gmail.com>
2025-09-22 15:05:14 +0100

    Changes to be committed: 	modified:   tests/udemy-tutorials/Section-23-Interview-Prep/Common_JavaScript_Interview_Questions_04.js

--- tests/udemy-tutorials/Section-23-Interview-Prep/Common_JavaScript_Interview_Questions_04.js
+++ tests/udemy-tutorials/Section-23-Interview-Prep/Common_JavaScript_Interview_Questions_04.js
@@ -158,8 +158,115 @@ Use "push" and "unshift" when you need to ADD elements to an array.
 Use "pop" and "shift" when you need to REMOVE elements from an array.
 Use "slice" when you need to COPY parts of an array without changing the original array.
 Use "splice" when you need to modify specific parts of an array, while also changing the original array.
-
-==========================
-     Example usage
-==========================
 */
+
+/*
+==========================
+    Example usage
+==========================
+
+Challenge:
+We have an array of numbers accending in multiples (steps) of X.
+There is a gap in the array (some elements are missing).
+The goal is to fill in the missing gap(s) with the correct multiples, using any of the array methods.
+
+Assume:
+1. You don't know where the gap(s) are (i.e. there could be gap(s) anywhere in the array, including in-between the first 2 elements).
+2. You don't know what the multiples are (i.e you don't know it's in multiples of 5).
+*/
+
+//            Index:  0  1   2   3   4   5   6   7
+const numbersArray = [5, 25, 30, 35, 50, 55, 60, 65];
+
+/*
+Because we assume we don't know what the multiple (step) is, we need to calculate it.
+The gap could be anywhere in the array, including the first 2 elements.
+This means we can't calcuate the step by simply writing "const step = numbersArray[1] - numbersArray[0]".
+
+We can calculate the step by finding the minimum difference between any two consecutive elements.
+The assumption is that the correct step is the smallest positive difference found between any pair of consecutive elements.
+*/
+
+function calculateStep(numbersArray) {
+  //First check that there are enough elements to determine the step:
+  if (numbersArray.length < 2) {
+    console.log(`Not enough elements to determine the step.`);
+    return 0;
+  }
+
+  //We need to work backwards (subtractively) with minStep.
+  //It helps that minStep is initialised as a large number, so infinity is a good option.
+  //Eventually minStep will be filtered down to the lowest positive difference.
+  let minStep = Infinity;
+
+  //Loop through the array and find the difference between each consecutive pair of elements.
+  //If the current difference is > 0, and < minStep, then assign the minStep to the difference.
+  for (let i = 0; i < numbersArray.length - 1; i++) {
+    let difference = numbersArray[i + 1] - numbersArray[i];
+    if (difference > 0 && difference < minStep) {
+      minStep = difference;
+    }
+  }
+  return minStep;
+}
+
+function fillGaps(numbersArray) {
+  //Grab the step first:
+  let step = calculateStep(numbersArray);
+  console.log(`step: ${step}.`); //5
+
+  for (let i = 0; i < numbersArray.length; i++) {
+    let currentElement = numbersArray[i];
+    let nextElement = numbersArray[i + 1];
+
+    //Calculate gap count (the amount of indexes with missing numbers):
+    let gapCount = (nextElement - currentElement) / step - 1;
+
+    if (gapCount > 0) {
+      //Initialise missingNumbers[] fresh for each potential gap in numbersArray[].
+      //If we didn't do this, we'd accumulate all missing numbers from all gaps into a single missingNumbers[] array.
+      //On encountering the first gap, we'd potentially insert all missing numbers from all gaps at once into the array for that first gap.
+      let missingNumbers = [];
+
+      for (let j = 1; j <= gapCount; j++) {
+        missingNumbers.push(j * step + currentElement); // ((j * step) + currentElement)
+
+        /*
+        Initial numbersArray = [5, 25, 30, 35, 50, 55, 60, 65];
+        
+        1st loop iteration:
+                gapCount = 3
+                currentElement = 5 (i = 0)
+                missingNumbers = [10, 15, 20]
+
+         2nd loop iteration:
+                gapCount = 2
+                currentElement = 35 (i = 3)
+                missingNumbers = [40, 45]
+        */
+      }
+
+      //Insert the missing numbers at i+1 position:
+      numbersArray.splice(i + 1, 0, ...missingNumbers);
+
+      /*
+      Initial numbersArray = [5, 25, 30, 35, 50, 55, 60, 65];
+
+        1st loop iteration:
+                currentElement = 5 (i+1 = 25)
+                numbersArray = [5, 10, 15, 20, 25, 30, 35, 50, 55, 60, 65]
+
+        2nd loop iteration:
+                currentElement = 35 (i+1 = 50)
+                numbersArray = 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65]
+        */
+
+      //Since we inserted elements, skip those in the loop to avoid an infinite loop:
+      i += gapCount;
+    }
+  }
+}
+
+console.log(`Before filling gap(s):`, numbersArray);
+fillGaps(numbersArray);
+console.log(`After filling gap(s):`, numbersArray);
